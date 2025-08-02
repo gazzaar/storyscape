414c5cc241fc6c6888a9e8ec3da604f105a1aa73
gazzaar <gazzarDev@gmail.com>
2025-08-02 10:52:27 +0300

    MUI

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,10 +1,16 @@
 import { Link } from '@tanstack/react-router';
+import Button from '@mui/material/Button';
 export const Header = () => {
   return (
     <>
       <h1>StorySpace</h1>
-      <Link to='/about'>About</Link>
-      <Link to='/'>About</Link>
+
+      <Link to='/posts'>
+        <Button>Posts</Button>
+      </Link>
+      <Link to='/'>
+        <Button>Home</Button>
+      </Link>
     </>
   );
 };
