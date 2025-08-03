ff81a082bc85a703dfacbdb8b07c41a5936e158a
gazzaar <gazzarDev@gmail.com>
2025-08-03 12:10:51 +0300

    Logger

--- server/middleware/loggerMiddleware.ts
+++ server/middleware/loggerMiddleware.ts
@@ -1,6 +1,11 @@
 import { RequestHandler } from 'express';
+import { LOGGER } from '../logging';
 
-export const requestloggerMiddleware: RequestHandler = (req, _res, next) => {
-  console.log(req.method, req.path, '- body:', req.body);
+export const requestloggerMiddleware: RequestHandler = (req, _, next) => {
+  LOGGER.info({
+    method: req.method,
+    path: req.path,
+    body: Object.keys(req.body).length ? req.body : undefined,
+  });
   next();
 };
